51a8baea53f59bec88edd5865f7ddecb9187a42f
WellingtonFidelis <wellingtonf20@gmail.com>
2022-09-25 15:58:07 -0300

    feat(nvimconfig): adding python language server.

--- plugin/lspconfig.rc.lua
+++ plugin/lspconfig.rc.lua
@@ -102,6 +102,10 @@ nvim_lsp.sumneko_lua.setup {
   },
 }
 
+nvim_lsp.pyright.setup {
+  on_attach = on_attach,
+}
+
 nvim_lsp.tailwindcss.setup {}
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
